22e4283de580cf4ff7bb553f19ccfd3c9334d196
StiliyanZakov <stanlizakov85@gmail.com>
2025-01-19 14:11:02 +0200

    Add routes

--- src/controllers/home-controller.js
+++ src/controllers/home-controller.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
-router.get('/', (req, res) => {
-    res.render('home');
-  }); 
+router.get("/", (req, res) => {
+  res.render("home");
+});
 
-  router.get('/about', (req, res) => {
-    res.render('about');
-  });
- 
+router.get("/about", (req, res) => {
+  res.render("about");
+});
+
 export default router;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
-import homeController from './controllers/home-controller.js';
+
+import routes from './routes.js';
+
 
 const app = express();
 
@@ -12,10 +14,8 @@ app.set('views', './src/views');
 
 app.use('/static',express.static('src/public'));
 
-app.use(homeController);
+app.use(routes);
 
-app.get('*', (req, res) => {
-  res.render('404');
-});
+
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
