1d73e793aab087cd08b4031dfff424f6720fdef7
StiliyanZakov <stanlizakov85@gmail.com>
2025-01-23 16:08:21 +0200

    Add search by genre and year

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -8,6 +8,12 @@ export default {
     if (filter.search) {
       result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
     }
+    if (filter.genre) {
+      result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
+    }
+    if (filter.year) {
+      result = result.filter(movie => movie.year === Number(filter.year));
+    }
 
     return result;
   },
