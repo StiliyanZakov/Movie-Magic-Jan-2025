be2bc6703db4ac5a2293fceecd81fce57ac2bb99
StiliyanZakov <stanlizakov85@gmail.com>
2025-01-25 17:15:05 +0200

    Config hendlebars to use mongoose document

--- src/index.js
+++ src/index.js
@@ -1,27 +1,49 @@
-import express from 'express';
-import handlebars from 'express-handlebars';
+import express from "express";
+import handlebars from "express-handlebars";
+import mongoose from "mongoose";
 
-import routes from './routes.js';
-import showRatingHelper from './helpers/rating-helper.js';
-
+import routes from "./routes.js";
+import showRatingHelper from "./helpers/rating-helper.js";
 
 const app = express();
 
-app.engine('hbs', handlebars.engine({ 
-  extname: 'hbs', 
-  helpers: {
-   showRating: showRatingHelper,
-    
-  }
-}));
-app.set('view engine', 'hbs');
-app.set('views', './src/views');
+// db configuration
+try {
+  const url = "mongodb://127.0.0.1:27017/magic-movies-jan-2025";
+  await mongoose.connect(url);
 
-app.use('/static',express.static('src/public'));
+  console.log("DB connected Successfully!");
+
+} catch (error) {
+  console.log('Cannot connect to DB');
+  console.error(error.message);
+
+}
+
+// handlebars configuration
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+      }, 
+    helpers: {
+      showRating: showRatingHelper,
+    },
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+
+// express configuration
+app.use("/static", express.static("src/public"));
 app.use(express.urlencoded({ extended: false }));
 
+//set up routes
 app.use(routes);
 
-
-
-app.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
+// start the server
+app.listen(5000, () =>
+  console.log("Server is listening on http://localhost:5000...")
+);
