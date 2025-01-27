fa38bd6c252132a1e0ecf9ff2d16ddfd4124e0ba
StiliyanZakov <stanlizakov85@gmail.com>
2025-01-27 09:56:46 +0200

    Add cast functionality

--- src/controllers/cast-controller.js
+++ src/controllers/cast-controller.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import castService from "../services/cast-service.js";
  
 
 const  castController = Router();
@@ -6,5 +7,13 @@ const  castController = Router();
 castController.get('/create', (req, res) => {
     res.render('cast/create');
     });
+
+castController.post('/create', async (req, res) => {
+    const castData = req.body;
+    
+    await castService.create(castData);
+
+    res.redirect('/');
+});
 
     export default castController;
