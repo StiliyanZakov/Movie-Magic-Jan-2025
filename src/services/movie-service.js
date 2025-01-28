3b43d4322cc2b46df3dc00d736c3eb0bf8ac0ab1
StiliyanZakov <stanlizakov85@gmail.com>
2025-01-29 00:50:37 +0200

    Update movie-service.js

--- src/services/movie-service.js
+++ src/services/movie-service.js
@@ -1,55 +1,56 @@
-import Movie from "../models/movie.js";
+import Movie from '../models/Movie.js';
 
-const movieService = {
-  getAll(filter = {}) {
+export default {
+    getAll(filter = {}) {
+        let query = Movie.find({});
 
-    let query = Movie.find({});
+        if (filter.search) {
+            // TODO: fix partial case insensitive search 
+            query = query.where({ title: filter.search });
+        }
 
-    if (filter.search) {
-      query = query.where({ title: filter.search });
+        if (filter.genre) {
+            // TODO: add case insensitive search
+            query = query.where({ genre: filter.genre });
+        }
 
+        if (filter.year) {
+            query = query.where({ year: Number(filter.year) })
+        }
+
+        return query;
+    },
+    getOne(movieId) {
+        const result = Movie.findById(movieId);
+
+        return result;
+    },
+    getOneWithCasts(movieId) {
+        return this.getOne(movieId).populate('casts');
+    },
+    create(movieData) {
+        const result = Movie.create({
+            ...movieData,
+            rating: Number(movieData.rating),
+            year: Number(movieData.year),
+        });
+
+        return result;
+    },
+    async attachCast(movieId, castId) {
+
+        // Attach #1
+        // const movie = await Movie.findById(movieId);
+        // if (movie.casts.includes(castId)) {
+        //     return;
+        // }
+
+        // movie.casts.push(castId);
+        // await movie.save();
+
+        // return movie;
+
+        // Attach #2
+        return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
     }
-
-    if (filter.genre) {
-      query = query.where({ genre: filter.genre });
-    }
-
-    if (filter.year) {
-      query = query.where({ year: Number(filter.year) });
-    }
-
-    return query;
-  },
-  getOne(movieId) {
-    const result = Movie.findById(movieId);
-
-    return result;
-  },
-
-  create(movieData) {
-    const result = Movie.create({
-      ...movieData,
-      rating: Number(movieData.rating),
-      year: Number(movieData.year),
-    });
-
-    return result;
-  },
- async attachCast(movieId, castId) {
-// Atach #1
-  const movie = await Movie.findById(movieId);
-  movie.casts.push(castId);
-  await movie.save();
-
-  return movie;
-
-  // Aatach #2
-
-  },
-  async getOneWithCasts(movieId) {
-    const movie = await Movie.findById(movieId).populate('casts');
-    return movie;
-  }
-};
-
-export default movieService;
+}
