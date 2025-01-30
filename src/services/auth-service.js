85beab52bd0ab620b723eb45e20c9682fd0f1c14
StiliyanZakov <stanlizakov85@gmail.com>
2025-01-30 19:45:52 +0200

    Add login functionality with jwt generation

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,7 +1,31 @@
 import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+
+const SECRET = '$4b$10$nlqYlsuRforsE4plC010A.fRrfTrRFWnOX9bjf8C.7ODhHPMq1FOQ';
 export default {
     register(userData) {
         return User.create(userData);
-    }
+    },
+    async login(email, password) {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            throw new Error("Invalid email or password");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new Error("Invalid email or password");
+        }
+
+        const payload = {
+            id: user.id,
+            email: user.email,
+        };
+        const token = jwt.sign(payload, SECRET, { expiresIn: '2h' });
+
+        return token;
+    },
 };
