38c04a92ed97d8791b57837ad1e1ce8a8bbb1fbe
StiliyanZakov <stanlizakov85@gmail.com>
2025-02-04 21:25:40 +0200

    Add  movie model validation

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,15 +1,48 @@
+import { populate } from 'dotenv';
 import { Schema, Types, model } from 'mongoose';
 
 // Create a schema
 const movieSchema = new Schema({
-    title: String,
+    title: {
+        type: String,
+        required: [true, 'Title is requred!'],
+        minLength: [5, 'Title should be at least 5 characters long!'],
+        maxLength: 50,
+        match: [/^[a-zA 0-9]+$/, 'Title should be alphanumeric, degits and whitespaces only!']
+    },
     category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    genre: {
+        type: String,
+        required: [true, 'Genre is requred!'],
+        minLength: [5, 'Genre should be at least 5 characters long!'],
+        maxLength: 50,
+        match: [/^[a-zA 0-9]+$/, 'Genre should be alphanumeric, degits and whitespaces only!']
+    },
+    director: {
+        type: String,
+         minLength: [5, 'Director should be at least 5 characters long!'],
+        maxLength: 50,
+        match: [/^[a-zA 0-9]+$/, 'Director should be alphanumeric, degits and whitespaces only!']
+    },
+    year: {
+        type: Number,
+        min: 1900,
+        max: 2025
+    },
+    imageUrl: {
+        type: String,
+        match: /^https?:\/\//,
+    },
+    rating: {
+    type: Number,
+    min: 1,
+    max:5
+    },
+    description: {
+        type: String,
+        minLength: 20,
+        match: /^[a-zA 0-9]+$/,
+    },
     casts: [{
         type: Types.ObjectId,
         ref: 'Cast'
